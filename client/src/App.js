d6659bf9b5657397ebe717b38768f8e1f7de02fb
sabataitis <sabataitisvilius@gmail.com>
2020-03-31 02:54:11 +0300

    BackEnd-> Update/Remove functionallity added. FrontEnd-> refacturing of the code(unfinished)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import ReactMapGL, { Marker, Popup, NavigationControl } from "react-map-gl";
-import { listLogEntries } from "./api";
-import { LogEntryForm } from "./LogEntryForm";
+import { listLogEntries, deleteLogEntry } from "./api";
+import { LogEntryForm } from "./Components/LogEntryForm";
+import { EditLogEntryForm } from "./Components/EditEntryForm";
+import { MarkerSvg } from "./Utils/MarkerSvg";
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
   const [showPopup, setShowPopup] = useState({});
   const [entryLocation, setEntryLocation] = useState(null);
+  const [editStatus, setEditStatus] = useState(false);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -13,39 +16,39 @@ const App = () => {
     longitude: 3.7492,
     zoom: 4
   });
-  async function getEntries() {
+  const showEditForm = () => {
+    setEditStatus(!editStatus);
+  };
+  const getEntries = async () => {
     const logEntries = await listLogEntries();
     setLogEntries(logEntries);
-  }
-  useEffect(() => {
-    getEntries();
-  }, []);
-  const showAddMarketPopup = event => {
+  };
+  const showAddMarkerPopup = event => {
     const [longitude, latitude] = event.lngLat;
     setEntryLocation({
       longitude: longitude,
       latitude: latitude
     });
   };
+  useEffect(() => {
+    getEntries();
+  }, [logEntries]);
   return (
     <ReactMapGL
       {...viewport}
       mapStyle="mapbox://styles/mapbox/streets-v11"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={setViewport}
-      onDblClick={showAddMarketPopup}
+      onDblClick={showAddMarkerPopup}
       doubleClickZoom={false}
     >
-      <div style={{ position: "absolute", right: 0 }}>
+      <div style={{ position: "absolute", right: 0, bottom: 0 }}>
         <NavigationControl />
       </div>
 
       {logEntries.map(logEntry => (
-        <React.Fragment key= {logEntry._id}>
-          <Marker
-            latitude={logEntry.latitude}
-            longitude={logEntry.longitude}
-          >
+        <React.Fragment key={logEntry._id}>
+          <Marker latitude={logEntry.latitude} longitude={logEntry.longitude}>
             <div
               onClick={() =>
                 setShowPopup({
@@ -53,22 +56,13 @@ const App = () => {
                 })
               }
             >
-              <svg
-                style={{
-                  width: ` ${6 * viewport.zoom}px`,
-                  height: ` ${6 * viewport.zoom}px`
-                }}
-                className="marker"
-                viewBox="0 0 24 24"
-                fill="none"
-              >
-                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                <circle cx="12" cy="10" r="3"></circle>
-              </svg>
+              <MarkerSvg viewport={viewport} />
             </div>
           </Marker>
           {showPopup[logEntry._id] ? (
             <Popup
+              captureScroll={true}
+              maxWidth={`200px`}
               latitude={logEntry.latitude}
               longitude={logEntry.longitude}
               closeButton={true}
@@ -81,18 +75,61 @@ const App = () => {
               }
               anchor="top"
             >
-              <div className = "popup">
+              <div className="popup">
                 <h3>{logEntry.title}</h3>
-                <p>{logEntry.comments}</p>
+                <p>{logEntry.description}</p>
                 <small>
                   Visit Date:
                   {new Date(logEntry.visitDate).toLocaleDateString()}
                 </small>
-                {logEntry.image? <img src = {logEntry.image} alt = {logEntry.title}/>:null}
+                {logEntry.image ? (
+                  <img src={logEntry.image} alt={logEntry.title} />
+                ) : null}
+                <button
+                  className="btn"
+                  onClick={() => {
+                    deleteLogEntry(logEntry._id);
+                    setShowPopup({
+                      [logEntry._id]: false
+                    });
+                    getEntries();
+                  }}
+                >
+                  Remove
+                </button>
+                <button
+                  className="btn"
+                  onClick={() => {
+                    showEditForm();
+                  }}
+                >
+                  {editStatus ? "Cancel" : "Edit"}
+                </button>
               </div>
+              {editStatus ? (
+                <Popup
+                  captureScroll={true}
+                  latitude={logEntry.latitude}
+                  longitude={logEntry.longitude}
+                  closeButton={true}
+                  closeOnClick={false}
+                  offsetLeft={`weirdGetAroundHere`}
+                  dynamicPosition={true}
+                  onClose={() => setEditStatus(false)}
+                  anchor="top"
+                >
+                  <EditLogEntryForm
+                    dataProp={logEntry}
+                    onClose={() => {
+                      setEditStatus(false);
+                      getEntries();
+                    }}
+                  />
+                </Popup>
+              ) : null}
             </Popup>
           ) : null}
-      </React.Fragment>
+        </React.Fragment>
       ))}
       {entryLocation ? (
         <>
@@ -116,6 +153,7 @@ const App = () => {
             </div>
           </Marker>
           <Popup
+            captureScroll={true}
             latitude={entryLocation.latitude}
             longitude={entryLocation.longitude}
             closeButton={true}
--- server/src/models/LogModel.js
+++ server/src/models/LogModel.js
@@ -12,14 +12,7 @@ const logSchema = new Schema({
     required: true,
   },
   description: String,
-  comments: String,
   image: String,
-  rating: {
-    type: Number,
-    min: 0,
-    max: 10,
-    default: 0,
-  },
   visitDate: {
     type: Date,
     required: true,
